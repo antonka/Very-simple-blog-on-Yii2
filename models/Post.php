9c8ceb168a9b838bb835412fe62bfb52b324a151
Koderov <nullvar@yandex.ru>
2016-09-21 01:36:28 +0300

    Added Post model

---
+++ models/Post.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "posts".
+ *
+ * @property integer $id
+ * @property string $title
+ * @property string $content
+ * @property string $created_at
+ */
+class Post extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'posts';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['title', 'content'], 'required'],
+            [['content'], 'string'],
+            [['created_at'], 'safe'],
+            [['title'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'title' => 'Title',
+            'content' => 'Content',
+            'created_at' => 'Created At',
+        ];
+    }
+}
