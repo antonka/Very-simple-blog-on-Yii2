7fd4d2ecb42555f1f549bb27c9c893e85879f001
Koderov <nullvar@yandex.ru>
2016-10-01 04:57:37 +0300

    Added markdown parser

--- views/public/post.php
+++ views/public/post.php
@@ -1,13 +1,13 @@
 <?php
 
 use yii\helpers\Html;
+use yii\helpers\Markdown;
 
 $this->title = $title;
 $this->params['breadcrumbs'][] = $this->title;
 
 ?>
 <div class="site-about">
-    <h1><?= Html::encode($title) ?></h1>
     <p><i><?= Html::encode($created_at) ?></i></p>
-    <p><?= Html::encode($content) ?></p>
+    <div><?= Markdown::process($content); ?></div>
 </div>
