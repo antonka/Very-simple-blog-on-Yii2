fcbd392a0e20f9dbb2ce2560a95615b43dcfebd9
Koderov <nullvar@yandex.ru>
2016-10-26 23:57:09 +0300

    Added Category model

---
+++ models/Category.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace app\models;
+
+class Category extends yii\db\ActiveRecord
+{
+    /**
+     * @return string
+     */
+    public static function tableName() 
+    {
+        return 'categories';
+    }
+    
+    /**
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            [['name'], 'required'],
+            [['name'], 'string', 'max' => 50], 
+        ];
+    }
+    
+    public function attributeLabels() 
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+        ];
+    }   
+}
