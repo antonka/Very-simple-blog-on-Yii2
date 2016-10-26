0b7436e5db8f2c81d332123685654ac63c8de88c
Koderov <nullvar@yandex.ru>
2016-10-26 23:42:32 +0300

    Added create_catagories_table migration

---
+++ migrations/m161026_203859_create_categories_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use yii\db\Migration;
+
+class m161026_203859_create_categories_table extends Migration
+{
+    /**
+     * @inheritdoc
+     */
+    public function up()
+    {
+        $this->createTable('categories', [
+            'id' => $this->primaryKey(),
+            'name' => $this->string(50)->notNull()
+        ]);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function down()
+    {
+        $this->dropTable('categories');
+    }
+}
