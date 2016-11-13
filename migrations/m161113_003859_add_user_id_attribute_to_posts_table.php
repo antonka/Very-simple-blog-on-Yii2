64a17ab8db1295867f062a84fb52393b74f631dd
Koderov <nullvar@yandex.ru>
2016-11-13 03:42:52 +0300

    Migration was added

---
+++ migrations/m161113_003859_add_user_id_attribute_to_posts_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * @author Anton Karamnov
+ */
+class m161113_003859_add_user_id_attribute_to_posts_table extends Migration
+{
+    public function up()
+    {
+        $this->addColumn('posts', 'user_id', $this->integer()->notNull());
+        $this->createIndex(
+            'posts_user_id', 
+            'posts', 
+            'user_id'
+        );
+        $this->addForeignKey(
+            'fk_posts_user_id', 
+            'posts', 
+            'user_id', 
+            'users', 
+            'id',
+            'CASCADE'
+        );
+    }
+
+    public function down()
+    {
+        $this->dropColumn('posts', 'user_id');
+    }
+}
