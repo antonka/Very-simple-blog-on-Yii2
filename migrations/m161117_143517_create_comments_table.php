44a2c2c65129fb73b59a6bddb2924e043a12e72e
Anton Karamnov <nullvar@yandex.ru>
2016-11-17 20:22:25 +0300

    create_comments_table migration was updated

--- migrations/m161117_143517_create_comments_table.php
+++ migrations/m161117_143517_create_comments_table.php
@@ -15,10 +15,39 @@ class m161117_143517_create_comments_table extends Migration
         $this->createTable('comments', [
             'id' => $this->primaryKey(),
             'user_id' => $this->integer()->notNull(),
+            'post_id' => $this->integer()->notNull(),
             'content' => $this->string(2000)->notNull(),
             'created_at' => $this->timestamp() . ' DEFAULT NOW()',
             'status' => 'ENUM("publicated", "moderation") NOT NULL',
         ], 'DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci');
+        
+        $this->createIndex(
+            'comments_user_id', 
+            'comments', 
+            'user_id'
+        );
+        $this->addForeignKey(
+            'fk_comments_user_id', 
+            'comments', 
+            'user_id', 
+            'users', 
+            'id',
+            'CASCADE'
+        );
+       
+        $this->createIndex(
+            'comments_post_id', 
+            'comments', 
+            'post_id'
+        );
+        $this->addForeignKey(
+            'fk_comments_post_id', 
+            'comments', 
+            'post_id', 
+            'posts', 
+            'id',
+            'CASCADE'
+        );
     }
 
     /**
