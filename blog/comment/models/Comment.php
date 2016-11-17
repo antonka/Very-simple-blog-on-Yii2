df7de3f5473795b22d9238051fee790f1c600ad8
Anton Karamnov <nullvar@yandex.ru>
2016-11-17 17:49:39 +0300

    Comment model was created

---
+++ blog/comment/models/Comment.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace blog\comment\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "comments".
+ *
+ * @property integer $id
+ * @property integer $user_id
+ * @property string $content
+ * @property string $created_at
+ * @property string $status
+ */
+class Comment extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'comments';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['user_id', 'content', 'status'], 'required'],
+            [['user_id'], 'integer'],
+            [['created_at'], 'safe'],
+            [['status'], 'string'],
+            [['content'], 'string', 'max' => 2000],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'user_id' => 'User ID',
+            'content' => 'Content',
+            'created_at' => 'Created At',
+            'status' => 'Status',
+        ];
+    }
+}
