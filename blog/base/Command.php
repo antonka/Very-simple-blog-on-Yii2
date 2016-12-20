1ae1872ea4cdd281a3494ff0df81b34c0fac6f40
Anton Karamnov <nullvar@yandex.ru>
2016-12-21 00:08:25 +0300

    getClassName method was added to Command class

--- blog/base/Command.php
+++ blog/base/Command.php
@@ -8,5 +8,13 @@ namespace blog\base;
 abstract class Command
 {
     abstract function run();
+    
+    /**
+     * @return string
+     */
+    public static function getClassName()
+    {
+        return get_called_class();
+    }
 }
 
