5595482e04023cdfd0d40f6fe4064af52c62e261
Anton Karamnov <nullvar@yandex.ru>
2016-12-21 00:09:06 +0300

    SendConfirmationEmail command was added

---
+++ blog/user/commands/SendConfirmationEmail.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace blog\user\commands;
+
+/**
+ * @author Anton Karamnov
+ */
+class SendConfirmationEmail extends \blog\base\Command
+{
+    public function run()
+    {
+    }
+}
+
