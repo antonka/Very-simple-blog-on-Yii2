97ecc104f0db5a7e406e29ab2c24ed581f7a9338
Anton Karamnov <nullvar@yandex.ru>
2016-12-20 23:55:39 +0300

    CommandFactory class was added

---
+++ blog/base/CommandFactory.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace blog\base;
+
+use blog\base\models\Task as Task;
+
+/**
+ * @author Anton Karamnov
+ */
+class CommandFactory
+{
+    /**
+     * @param Task $task
+     */
+    public static function build(Task $task)
+    {
+        // build a command
+    }
+}
+
