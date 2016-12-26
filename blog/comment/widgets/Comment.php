4b7a233ebba5c2fa9ba53c5867bca20ed12ba90a
Anton Karamnov <nullvar@yandex.ru>
2016-12-27 00:12:26 +0300

    Comment widget was refactored

---
+++ blog/comment/widgets/Comment.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace blog\comment\widgets;
+
+use blog\comment\CommentFormFactory as CommentFormFactory;
+use Yii;
+
+
+/**
+ * @author Anton Karamnov
+ */
+class Comment extends \yii\base\Widget
+{
+    /**
+     * @var integer
+     */
+    public $postId;
+    
+    /**
+     * @var blog\comment\models\CommentForm
+     */
+    public $commentForm;
+    
+    public function init() 
+    {
+        if (is_null($this->commentForm)) {
+            $this->commentForm = CommentFormFactory::getCommentForm();
+        }
+        
+        if (!empty($this->postId)) {
+            $this->commentForm->postId = $this->postId;
+        }
+    }
+
+    public function run()
+    {
+        return $this->render('comment_box', [
+            'commentForm' => $this->commentForm,
+        ]);
+    }    
+}
+
+
