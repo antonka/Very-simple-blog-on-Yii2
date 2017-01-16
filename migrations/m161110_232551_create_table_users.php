9032989dc9c880bfdd4bf8a45f0eff96b1ff7da1
Anton Karamnov <nullvar@yandex.ru>
2017-01-17 00:12:45 +0300

    create_table_users migration was updated

--- migrations/m161110_232551_create_table_users.php
+++ migrations/m161110_232551_create_table_users.php
@@ -13,8 +13,8 @@ class m161110_232551_create_table_users extends Migration
             'id' => $this->primaryKey(),
             'email' => $this->string(255)->notNull(),
             'name' => $this->string(50)->notNull(),
-            'password' => $this->string(64)->null(),
-            'role' => 'ENUM("owner", "commentator") NOT NULL',
+            'password' => $this->string(64)->notNull(),
+            'role' => $this->string(50)->notNull(),
         ], 'DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci');
     }
 
