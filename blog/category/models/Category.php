a45576f13fd18879a8b5ff441f11aa98f10a3474
Anton Karamnov <nullvar@yandex.ru>
2017-04-10 23:01:11 +0300

    A unnecessary code was removed

--- blog/category/models/Category.php
+++ blog/category/models/Category.php
@@ -42,14 +42,4 @@ class Category extends \blog\base\ActiveRecord
         return $this->hasMany(\blog\post\models\Post::className(), ['id' => 'post_id'])
             ->viaTable('posts_categories', ['category_id' => 'id']);
     }
-    
-    /**
-     * @return array
-     */
-    public static function getAllIds()
-    {
-        return Yii::$app->db->createCommand(
-            'SELECT id FROM ' . self::tableName()
-        )->queryColumn();
-    }
 }
